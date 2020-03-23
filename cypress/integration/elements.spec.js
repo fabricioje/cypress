6adfe150c0f4b39feeb729002060649dc71685e4
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-23 20:45:24 -0300

    - Hooks

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,11 +1,29 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () =>{
-    it('Text', () =>{
-        cy.visit('https://wcaquino.me/cypress/componentes.html')
 
+    //Executa uma vez antes do teste começar
+    before(() =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    //Executa antes de cada teste
+    beforeEach(() =>{
+        cy.reload()
+    })
+
+    it('Text', () =>{
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+    })
+
+    it('Links', () =>{
+        //cy.get('[href="#"]').click()
+        //cy.get('#resultado').should('have.value', 'Voltou!')
+
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
     })
 })
