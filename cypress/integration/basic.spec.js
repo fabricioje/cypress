2bbdeacd5504cfdd30a07862de88789f2289a300
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-23 15:25:11 -0300

    - Textos

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Cypress basic', () => {
-    it('Should visit a page and assert title', () =>{
+    it.only('Should visit a page and assert title', () =>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         //const title = cy.title()
         //console.log(title)
+
+        cy.pause()
 
         cy.title().should('to.be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
@@ -15,7 +17,7 @@ describe('Cypress basic', () => {
             .and('contain', 'Campo')
     })
 
-    it.only('Shoul find and interact with an element', () =>{
+    it('Shoul find and interact with an element', () =>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         cy.get('#buttonSimple')
