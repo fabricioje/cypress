950601fde3b1bd7ca077708de93d668de275f510
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-24 12:17:13 -0300

    - Entendo espera no Cypress

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Esperas...' ,() => {
+    //Executa uma vez antes do teste começar
+    before(() =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    //Executa antes de cada teste
+    beforeEach(() =>{
+        cy.reload()
+    })
+
+    it('Deve aguardar elemento estar disponiveis', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funciona')
+    })
+})
