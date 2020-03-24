49f3e3dd6f0d9a670f8c6a7634aaa65fee83d3f1
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-24 17:30:17 -0300

    - Sincronizando o titulo

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -7,7 +7,7 @@ describe('Cypress basic', () => {
         //const title = cy.title()
         //console.log(title)
 
-        cy.pause()
+        //cy.pause()
 
         cy.title().should('to.be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
@@ -15,6 +15,10 @@ describe('Cypress basic', () => {
         cy.title()
             .should('to.be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
+        
+        cy.title().then(title => {
+            console.log(title)
+        })
     })
 
     it('Shoul find and interact with an element', () =>{
