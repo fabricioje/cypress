2b944fdd7f87175ade88dd759701213919bcd1e8
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-24 10:45:59 -0300

    - Texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -26,4 +26,24 @@ describe('Work with basic elements', () =>{
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
+
+    it.only('TextFields', () =>{
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes').type('text area')
+        cy.get('#elementosForm\\:sugestoes').should('have.value', 'text area')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('text area')
+            .should('have.value', 'text area')
+            
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay:100})
+            .should('have.value', 'acerto')
+    })
 })
