413cb7450758f7b94264be5456f76cdf0aba5720
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-25 15:00:28 -0300

    - Invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -23,4 +23,18 @@ describe('Helpers...', () => {
         cy.get("#buttonList").then(() => console.log("Encontrei o segundo botão"))
 
     })
+
+    it.only('Invoke...', () =>{
+        const getValue = () => 1;
+        const soma = (a, b) => a + b;
+
+        cy.wrap({fn: getValue}).invoke('fn').should('be.equal', 1);
+        cy.wrap({fn: soma}).invoke('fn', 2, 5).should('be.equal', 7)
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'Dá para ver?')
+        cy.get('#resultado')
+            .invoke('html', '<input type="button" value="hached">')
+    })
 })
