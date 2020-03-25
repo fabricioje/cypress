d0d97dec548b40158eaed117f125ad99ed5b01ae
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-25 19:29:31 -0300

    - PopUp

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -20,7 +20,37 @@ describe('Work with Popup', () =>{
         cy.get('@winOpen').should('be.called')
 
     })
+})
 
+describe('With links', () =>{
 
+    before(() =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
 
+    //Executa antes de cada teste
+    beforeEach(() =>{
+        cy.reload()
+    })
+
+    it.only('Check popup url', () => {
+        cy.contains('Popup2')
+            .should('have.prop', 'href')
+            .and('equal', 'https://wcaquino.me/cypress/frame.html')
+    })
+
+    it.only('Should access popup dinamically', () => {
+        cy.contains('Popup2').then($a => {
+            const href = $a.prop('href')
+            cy.visit(href)
+            cy.get('#tfield').type('funciona')
+        })
+    })
+
+    it.only('Should force link on same page', () => {
+        cy.contains('Popup2')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.get('#tfield').type('funciona')
+    })
 })
