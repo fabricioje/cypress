8c2e77d20a537928a50a4da691e3c2fadedb1fdf
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-25 16:38:42 -0300

    - Elementos

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -15,9 +15,23 @@ describe('Cypress basic', () => {
         cy.title()
             .should('to.be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
+
+            let syncTitle
         
         cy.title().then(title => {
             console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle= title
+        })
+
+        cy.get('[data-cy=dataSobrenome]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
         })
     })
 
--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -76,6 +76,16 @@ describe('Work with basic elements', () =>{
             .select('1graucomp')
             .should('have.value', '1graucomp')
         
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function () {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })
         //TODO Validar as opções do combo
     })
 
@@ -84,5 +94,14 @@ describe('Work with basic elements', () =>{
             .select(['natacao', 'Corrida', 'nada'])
         
         //TODO validar opções selecionadas do combo multiplo
+        cy.get('[data-testid=dataEsportes]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
+
     })
 })
