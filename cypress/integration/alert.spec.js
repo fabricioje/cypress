8485eac955cf472ab25d5f1928c8e14c01ac417f
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-25 17:05:54 -0300

    - Confirm

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -20,11 +20,36 @@ describe('Work with basic elements', () =>{
         })
     })
 
-    it.only('Alert com Mock', () => {
+    it('Alert com Mock', () => {
         const stub = cy.stub().as('alerta')
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledOnceWith('Alert Simples')
+        })
+    })
+
+    it('Alert', () => {
+        cy.get('#confirm').click()
+        cy.on('window:confirm', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirmado')
+        })
+    })
+
+    it.only('Denny', () => {
+        cy.get('#confirm').click()
+        cy.on('window:confirm', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Negado')
         })
     })
 })
