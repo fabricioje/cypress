afde29dd3a0b7e0712c12b7ed47646070a1e7c4f
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-25 18:54:07 -0300

    - PopUp

---
+++ cypress/integration/popup.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Work with Popup', () =>{
+
+    it.only('Deve testar o popup diretamente', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click()
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!')
+        })
+
+    })
+
+    it.only('Deve se o popup foi invocado', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.window().then(win => {
+            cy.stub(win, 'open').as('winOpen')
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should('be.called')
+
+    })
+
+
+
+})
