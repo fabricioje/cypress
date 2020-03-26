0c49d53a5f5ee2d4e0845dace78f68f6e8df3ef4
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-26 15:07:50 -0300

    - Clock

---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () =>{
+
+    //Executa uma vez antes do teste começar
+    before(() =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    //Executa antes de cada teste
+    beforeEach(() =>{
+        cy.reload()
+    })
+
+    it('Going back to past', () => {
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '26/03/2020')
+
+        // cy.clock()
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '10/04/2012')
+    })
+})
