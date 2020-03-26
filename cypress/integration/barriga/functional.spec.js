4ac0c72c2f2cf384bd9ecb04e1be5f22126e6486
Fabrício J. Silva <fabricio_fx@hotmail.com>
2020-03-26 18:07:48 -0300

    - Alterando uma conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -21,6 +21,19 @@ describe('Should test at a functional level', () =>{
         cy.get('[href="/contas"]').click()
         cy.get('[data-test=nome]').type('Conta de teste')
         cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'inserida com sucesso')
+        cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    })
+
+    it('Should update an account', () => {
+
+        cy.get('[data-test=menu-settings]').click()
+        cy.get('[href="/contas"]').click()
+
+        cy.xpath("//table//td[contains(. ,'Conta de teste')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test=nome]')
+            .clear()
+            .type('Conta alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta atualizada')
     })
 })
